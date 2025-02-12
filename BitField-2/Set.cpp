4e0d1ebb370e1edf99ef8753a1a7755635f208e6
karinaamos <184amos8a@mail.ru>
2025-02-12 13:25:01 +0300

    12.02.25

--- BitField-2/Set.cpp
+++ BitField-2/Set.cpp
@@ -1,65 +1,64 @@
 #include "Set.h"
 #include <math.h>
+#include "BitField.h"
+#include <vector>
 
-Set::Set(size_t mp) : _bitField(mp), _maxPower(mp){
+Set::Set(size_t mp):_bitField(mp){
+    _maxPower = mp;
 }
+Set::Set(const Set &set): _bitField(set._bitField), _maxPower(set._maxPower){}       // конструктор копирования
+Set::Set(const BitField &bf): _bitField(bf),  _maxPower(bf.GetLength()){} // конструктор преобразования типа
 
-Set::Set(const Set& s) : _bitField(s._bitField), _maxPower(s._maxPower){
+// доступ к битам
+size_t Set::GetMaxPower(void) const{    // максимальная мощность множества
+    return _maxPower;
 }
-
-Set::Set(const BitField& bf)  : _bitField(bf), _maxPower(bf.GetLength()){
-}
-
-void Set::InsElem(uint64_t Elem){
+void Set::InsElem(const uint64_t Elem){       // включить элемент в множество
     _bitField.SetBit(Elem);
 }
-
-void Set::DelElem(uint64_t Elem){
+void Set::DelElem(const uint64_t Elem){       // удалить элемент из множества
     _bitField.ClrBit(Elem);
 }
-
-bool Set::IsMember(uint64_t Elem) const{
+bool Set::IsMember(const uint64_t Elem) const { // проверить наличие элемента в множестве
     return _bitField.GetBit(Elem);
 }
-
-bool Set::operator==(const Set& s) const{
+// теоретико-множественные операции
+bool Set::operator== (const Set &s) const{ // сравнение
     return _bitField == s._bitField;
 }
-
-bool Set::operator!= (const Set &s) const{
-    return (_bitField == s._bitField) == 0;
+bool Set::operator!= (const Set &s) const{ // сравнение
+    if (_bitField == s._bitField){
+        return false;
+    }
+    return true;
 }
-
-Set& Set::operator=(const Set& tmp){
-    _maxPower = tmp._maxPower;
-    _bitField = tmp._bitField;
+Set& Set::operator=(const Set& s){  // присваивание
+    _maxPower = s.GetMaxPower();
+    _bitField = s._bitField;
     return *this;
 }
-
-Set Set::operator+(uint64_t Elem){
-    Set result(*this);
-    result.InsElem(Elem);
-    return result;
+Set Set::operator+ (const uint64_t Elem){ // объединение с элементом
+                                   // элемент должен быть из того же универса
+    Set s(*this);
+    s.InsElem(Elem);
+    return s;
 }
-
-Set Set::operator-(uint64_t Elem){
-    Set result(*this);
-    result.DelElem(Elem);
-    return result;
+Set Set::operator- (const uint64_t Elem){ // разность с элементом
+                                   // элемент должен быть из того же универса
+    Set s(*this);
+    s.DelElem(Elem);
+    return s;
 }
-
-Set Set::operator+(const Set& s){
+Set Set::operator+ (const Set& s){  // объединение
     return _bitField | s._bitField;
 }
-
-Set Set::operator*(const Set& s){
+Set Set::operator* (const Set &s){  // пересечение
     return _bitField & s._bitField;
 }
-
-Set Set::operator~(){
+Set Set::operator~ (){          // дополнение
     return Set(~_bitField);
 }
-
+// Выдать простые числа множества
 std::vector<uint64_t> Set::GetPrimary(){
     BitField f = _bitField;
     std::vector<uint64_t> array;
--- BitField-2/Set.h
+++ BitField-2/Set.h
@@ -9,9 +9,11 @@ private:
   BitField _bitField; // битовое поле для хранения характеристического вектора
 public:
   Set(size_t mp);
-  Set(const Set &s);       // конструктор копирования
+  Set(const Set &set);       // конструктор копирования
   Set(const BitField &bf); // конструктор преобразования типа
-  operator BitField(); // преобразование типа к битовому полю
+  operator BitField(){; // преобразование типа к битовому полю
+    return _bitField;
+  }
   // доступ к битам
   size_t GetMaxPower(void) const;     // максимальная мощность множества
   void InsElem(const uint64_t Elem);       // включить элемент в множество
